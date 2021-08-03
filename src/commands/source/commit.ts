f39e6c5e305fcb2fdb6a27b343d9dc20800202c9
mshanemc <shane.mclaughlin@salesforce.com>
2021-08-03 17:47:27 -0500

    feat: basic using isogit

--- src/commands/source/commit.ts
+++ src/commands/source/commit.ts
@@ -12,14 +12,8 @@ import {
 import { AnyJson } from '@salesforce/ts-types';
 import { ShadowRepo } from '../../lib/repo';
 
-// array members for status results
-// https://isomorphic-git.org/docs/en/statusMatrix#docsNav
-const FILE = 0;
-const HEAD = 1;
-const WORKDIR = 2;
-
-export default class SourceStatus extends SfdxCommand {
-  public static description = 'get local changes';
+export default class SourceCommit extends SfdxCommand {
+  public static description = 'commit local changes';
   protected static flagsConfig = {};
   protected static requiresUsername = true;
   protected static requiresProject = true;
@@ -31,21 +25,14 @@ export default class SourceStatus extends SfdxCommand {
         .map((dir) => dir.path)
         .join(',')}`
     );
+
     const shadowRepo = await ShadowRepo.create({
       orgId: this.org.getOrgId(),
       projectPath: this.project.getPath(),
       packageDirs: this.project.getPackageDirectories(),
     });
-    const changes = await shadowRepo.getChangedRows();
+    const sha = await shadowRepo.commitChanges();
 
-    const deletes = await shadowRepo.getDeleteFilenames();
-    const modifies = changes.filter((file) => file[HEAD] === 1 && file[WORKDIR] === 2).map((file) => file[FILE]);
-    const adds = changes.filter((file) => file[HEAD] === 0 && file[WORKDIR] === 2).map((file) => file[FILE]);
-    const output = {
-      deletes,
-      adds,
-      modifies,
-    };
-    return output;
+    return sha;
   }
 }
--- src/commands/source/init.ts
+++ src/commands/source/init.ts
@@ -5,18 +5,13 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import * as path from 'path';
-
 import {
   // flags,
   SfdxCommand,
 } from '@salesforce/command';
 // import { SfdxError } from '@salesforce/core';
 import { AnyJson } from '@salesforce/ts-types';
-import { fs } from '@salesforce/core';
-
-import { Repository } from 'nodegit';
-import { getRepoPath } from '../../lib/repo';
+import { ShadowRepo } from '../../lib/repo';
 
 export default class SourceInit extends SfdxCommand {
   public static description = 'initiate Source Tracking';
@@ -25,19 +20,14 @@ export default class SourceInit extends SfdxCommand {
   protected static requiresProject = true;
 
   public async run(): Promise<AnyJson> {
-    const repoPath = await getRepoPath();
-    const gitDir = path.join(repoPath, '.sfdx', 'orgs', this.org.getOrgId());
-    await fs.mkdirp(gitDir);
-    await Repository.initExt(gitDir, {
-      description: 'sfdx-source-tracking',
-      flags: null,
-      mode: null,
-      workdirPath: '.',
-      version: null,
-      templatePath: null,
-      originUrl: null,
-      initialHead: 'main',
+    const shadowRepo = await ShadowRepo.create({
+      orgId: this.org.getOrgId(),
+      projectPath: this.project.getPath(),
+      packageDirs: this.project.getPackageDirectories(),
     });
-    return undefined;
+    await shadowRepo.gitInit();
+
+    this.ux.log(`initialized git repo for ${shadowRepo.projectPath} in ${shadowRepo.gitDir}`);
+    return {};
   }
 }
