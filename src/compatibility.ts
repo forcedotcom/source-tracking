4d3380c74a72572a8c1e464c739c4d5b8af69442
Willie Ruemmele <willieruemmele@gmail.com>
2022-03-07 13:13:10 -0700

    fix: revert beta/legacy mapping

--- src/compatibility.ts
+++ src/compatibility.ts
@@ -104,9 +104,9 @@ export const replaceRenamedCommands = (input: string, reverse = false): string =
 };
 
 const renames = new Map([
-  ['force:source:status', 'force:source:legacy:status'],
-  ['force:source:push', 'force:source:legacy:push'],
-  ['force:source:pull', 'force:source:legacy:pull'],
-  ['force:source:tracking:reset', 'force:source:legacy:tracking:reset'],
-  ['force:source:tracking:clear', 'force:source:legacy:tracking:clear'],
+  ['force:source:status', 'force:source:beta:status'],
+  ['force:source:push', 'force:source:beta:push'],
+  ['force:source:pull', 'force:source:beta:pull'],
+  ['force:source:tracking:reset', 'force:source:beta:tracking:reset'],
+  ['force:source:tracking:clear', 'force:source:beta:tracking:clear'],
 ]);
