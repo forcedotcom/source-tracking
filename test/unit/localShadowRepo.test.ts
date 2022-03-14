fbc6d33fb363b514ddeee21bea539df465a5a3fc
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2022-03-14 07:10:39 -0700

    perf: deduplicate files as they are commited to shadow repo (#133)
    Co-authored-by: Kevin Jones <kjones@financialforce.com>

---
+++ test/unit/localShadowRepo.test.ts
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import * as path from 'path';
+import * as os from 'os';
+import * as fs from 'fs';
+import * as git from 'isomorphic-git';
+import { expect } from 'chai';
+import sinon = require('sinon');
+import { ShadowRepo } from '../../src/shared/localShadowRepo';
+
+/* eslint-disable no-unused-expressions */
+
+afterEach(() => {
+  // Restore the default sandbox here
+  sinon.restore();
+});
+
+describe('localShadowRepo', () => {
+  it('does not add same file multiple times', async () => {
+    let projectDir: string;
+    try {
+      projectDir = fs.mkdtempSync(path.join(os.tmpdir(), 'localShadowRepoTest'));
+      fs.mkdirSync(path.join(projectDir, 'force-app'));
+      fs.writeFileSync(path.join(projectDir, 'force-app', 'CustomLabels.labels-meta.xml'), '');
+
+      const shadowRepo: ShadowRepo = await ShadowRepo.getInstance({
+        orgId: '00D456789012345',
+        projectPath: projectDir,
+        packageDirs: [
+          {
+            name: 'dummy',
+            fullPath: 'dummy',
+            path: path.join(projectDir, 'force-app'),
+          },
+        ],
+      });
+
+      const gitAdd = sinon.spy(git, 'add');
+
+      const labelsFile = path.join('force-app', 'CustomLabels.labels-meta.xml');
+      const sha = await shadowRepo.commitChanges({ deployedFiles: [labelsFile, labelsFile] });
+
+      expect(sha).to.not.be.empty;
+      expect(gitAdd.calledOnce).to.be.true;
+    } finally {
+      if (projectDir) fs.rmdirSync(projectDir, { recursive: true });
+    }
+  });
+});
