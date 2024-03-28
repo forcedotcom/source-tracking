63faa2a9bcf12712855090730bb2b09d1206485f
mshanemc <shane.mclaughlin@salesforce.com>
2024-03-28 11:07:46 -0500

    chore: clearer exports

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-export * from './sourceTracking';
+export { SourceTracking, SourceTrackingOptions } from './sourceTracking';
 export {
   RemoteSyncInput,
   ChangeOptionType,
