361940fac6b8890f4887c84b40396815ee2762a9
Eric Willhoit <ewillhoit@salesforce.com>
2024-05-13 11:24:14 -0500

    test: temp logs

--- src/shared/localShadowRepo.ts
+++ src/shared/localShadowRepo.ts
@@ -546,8 +546,8 @@ const compareTypes = (
     const resolvedAdded = resolveType(resolverAdded, [addedFile])[0];
     const resolvedDeleted = resolveType(resolverDeleted, [deletedFile])[0];
 
-    console.log('resolvedAdded', resolvedAdded);
-    console.log('resolvedDeleted', resolvedDeleted);
+    logger.fatal('resolvedAdded', resolvedAdded);
+    logger.fatal('resolvedDeleted', resolvedDeleted);
 
     if (!resolvedAdded || !resolvedDeleted) {
       logger.warn(`Unable to resolve type for '${addedFile}' or '${deletedFile}'`);
@@ -559,8 +559,9 @@ const compareTypes = (
       continue;
     }
 
-    console.log('resolvedAdded.parent', resolvedAdded.parent);
-    console.log('resolvedDeleted.parent', resolvedDeleted.parent);
+    logger.fatal('resolvedAdded.parent', resolvedAdded.parent);
+    logger.fatal('resolvedDeleted.parent', resolvedDeleted.parent);
+
     // Name of the parent needs to match, we do not want to allow moving a child to a different parent
     // For example a custom field from one object to another (Broker__c to Account__c)
     if (resolvedAdded.parent?.name !== resolvedDeleted.parent?.name) {
