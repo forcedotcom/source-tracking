2a355888d71d2ec6162fe51b3113ebc96230e34b
mingxuanzhang <mingxuanzhang@salesforce.com>
2024-10-23 11:31:27 -0700

    ci: checks for bundling

---
+++ scripts/esbuild.config.js
@@ -0,0 +1,30 @@
+/**
+ * NOTE: This file does NOT really generate a bundle version of source-tracking
+ * source-tracking is bundled directly in salesforcedx-vscode
+ * The file is only used to detect any potential risks to esbuild.
+ **/
+const { build } = require('esbuild');
+
+const sharedConfig = {
+  bundle: true,
+  format: 'cjs',
+  platform: 'node',
+  external: [], // The whitelist of dependencies that are not bundle-able
+  keepNames: true,
+  plugins: [],
+  supported: {
+    'dynamic-import': false,
+  },
+  logOverride: {
+    // 'unsupported-dynamic-import': 'error',
+    // dynamic import is used in oclif to refer to a file path, which is legit.
+  },
+};
+
+(async () => {
+  await build({
+    ...sharedConfig,
+    entryPoints: ['./lib/index.js'],
+    outdir: 'dist',
+  });
+})();
