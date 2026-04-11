f817b1ff85334694997fcdcbcc5aac8a1db6b51f
mshanemc <shane.mclaughlin@salesforce.com>
2026-04-11 11:35:43 -0500

    chore: exclude nut fixture from lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
   extends: ['eslint-config-salesforce-typescript', 'eslint-config-salesforce-license', 'plugin:sf-plugin/library'],
   // ignore eslint files in NUT test repos
-  ignorePatterns: ['test/nuts/ebikes-lwc'],
+  ignorePatterns: ['test/nuts/ebikes-lwc', 'test/nuts/repros/reactinternalapp'],
 };
