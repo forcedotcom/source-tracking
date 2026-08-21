cbaa635c4c96188164ba2cf6260d9130646bf93f
Jamie Feingold <jfeingold@salesforce.com>
2026-08-21 08:36:37 -0500

    fix: rejiggered eslint.config.mjs (W-23928423)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,1 +1,12 @@
-export { default } from 'eslint-config-salesforce-typescript';
+import tsconfigs from 'eslint-config-salesforce-typescript';
+import plugin from 'eslint-plugin-sf-plugin';
+
+const configs = [
+  {
+    ignores: ['test/nuts/ebikes-lwc', 'test/nuts/repros/reactinternalapp'],
+  },
+  ...tsconfigs,
+  ...plugin.configs.library,
+];
+
+export default configs;
